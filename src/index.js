ed80df1aa1689f8afbbf5359be75cdcf04179e64
Lorenzo <lore.mail.gl@gmail.com>
2025-08-15 16:29:19 +0200

    attenzione modifica con creazione codici invece di token per accesso a google backend vercel.. versione fixata 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
-    <App />
+     <GoogleOAuthProvider clientId="954741971381-4qtl2v6f4b2iebt23kd827sumf6d31dg.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   //</React.StrictMode>
 );
 
