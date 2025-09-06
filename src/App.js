d5b47831de1e7a9a8c498cabbc4fecd39b65c76c
Lorenzo <lore.mail.gl@gmail.com>
2025-09-06 11:05:20 +0200

    Refactor: cambiata root di agentdemo in /exam e cambiata navigazione da homepage ad agent demo su login page v2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
 // 1. RIMUOVI: DayTopicsSelector non serve più
 // import DayTopicsSelector from './components/DayTopicsSelector'; 
@@ -26,30 +25,20 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Route pubblica di login */}
-        <Route path="/" element={
-          user ? <Navigate to="/homepage" replace /> : <LoginPage />
-        } />
 
         {/* Routes protette - accessibili solo se autenticati */}
         {user ? (
           <>
-            <Route path="/homepage" element={<HomePage />} />
-
-
-            {/* NUOVA ROTTA per l'agente */}
-            {/* Gli passiamo projectId e topicId per sapere quale PDF caricare <Route path="/projects/:projectId/topic/:topicId/exam" element={<AgentDemo />} />*/}
-            
             <Route path="/exam" element={<AgentDemo />} />
           </>
         ) : (
           /* Se non autenticato, redirect alla login */
-          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path="/" element={<LoginPage />} />
         )}
 
         {/* Fallback per routes non esistenti */}
         <Route path="*" element={
-          <Navigate to={user ? "/homepage" : "/"} replace />
+          <Navigate to={user ? "/exam" : "/"} replace />
         } />
       </Routes>
     </Router>
