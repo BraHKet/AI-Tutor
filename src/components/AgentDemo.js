1ae11499dc05473216e3b9a2b9b9721565a0ceea
Lorenzo R <lore.mail.gl@gmail.com>
2025-09-24 19:36:43 +0200

    FIN QUA PERFETTO!!!!!!!! Modifica css pulsanti di disegno V36

--- src/components/AgentDemo.js
+++ src/components/AgentDemo.js
@@ -1058,6 +1058,7 @@ useEffect(() => {
         message="Il professore sta rispondendo..."
         size="medium"
         fullScreen={true}  
+        style={{ backgroundColor: "rgba(255, 255, 255, 0.3)" }}
       />
     );
   }
